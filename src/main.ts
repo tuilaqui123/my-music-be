cd5abeec17e8826c2da61f97c9a5dee54af09ad1
QuiPham <qui.pham@dotb.vn>
2025-09-04 13:31:24 +0700

    change path for thumnail and music

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'reflect-metadata';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/thumbnails', express.static('D:/my-music-data/thumbnails'));
+  app.use('/musics', express.static('D:/my-music-data/musics'));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Bỏ field không hợp lệ
@@ -13,6 +18,6 @@ async function bootstrap() {
   );
   app.enableCors(); // Bật CORS nếu cần thiết
   // app.setGlobalPrefix('api'); // Thiết lập tiền tố toàn cục cho các route
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
--- src/modules/tracks/service/tracks.service.ts
+++ src/modules/tracks/service/tracks.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/service/base.service';
 import { TracksEntity } from '../entity/tracks.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TracksService extends BaseService<TracksEntity> {}
+export class TracksService extends BaseService<TracksEntity> {
+  constructor(
+    @InjectRepository(TracksEntity)
+    repository: Repository<TracksEntity>,
+  ) {
+    super(repository);
+  }
+}
